40152a786120e2cef060ab622610841681c513a0
luo <2407248257@qq.com>
2023-04-02 21:07:10 +0800

    登录注销操作

--- src/main/java/com/intellectrecovery/controller/DoctorController.java
+++ src/main/java/com/intellectrecovery/controller/DoctorController.java
@@ -1,9 +1,46 @@
 package com.intellectrecovery.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.intellectrecovery.domain.Doctor;
+import com.intellectrecovery.domain.Result;
+import com.intellectrecovery.service.DoctorService;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/doctor")
 public class DoctorController {
+
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    DoctorService doctorService;
+
+    @PostMapping("/login")
+    public Result login(@RequestBody Doctor doctor) {
+        return null;
+    }
+
+    @DeleteMapping("/exit")
+    public Result exit(@RequestBody String username) {
+        return null;
+    }
+
+    @PostMapping("/register")
+    public Result register(@RequestBody Doctor doctor) {
+        return null;
+    }
+
+    @GetMapping("/getUser/{username}")
+    public Result getDoctorByUsername(@PathVariable String username) {
+        return null;
+    }
+
+    @PutMapping("/changePassword")
+    public Result changePassword() {
+        return null;
+    }
+
 }
--- src/main/java/com/intellectrecovery/controller/UserController.java
+++ src/main/java/com/intellectrecovery/controller/UserController.java
@@ -1,9 +1,46 @@
 package com.intellectrecovery.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.intellectrecovery.domain.Result;
+import com.intellectrecovery.domain.User;
+import com.intellectrecovery.service.UserService;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    UserService userService;
+
+    @PostMapping("/login")
+    public Result login(@RequestBody User user) {
+        return null;
+    }
+
+    @DeleteMapping("/exit")
+    public Result exit(@RequestBody String username) {
+        return userService.exit(username);
+    }
+
+    @PostMapping("/register")
+    public Result register(@RequestBody User user) {
+        return null;
+    }
+
+    @GetMapping("/getUser/{username}")
+    public Result getUserByUsername(@PathVariable String username) {
+        return null;
+    }
+
+    @PutMapping("/changePassword")
+    public Result changePassword() {
+        return null;
+    }
+
 }
