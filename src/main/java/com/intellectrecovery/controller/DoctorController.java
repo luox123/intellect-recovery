dccb63c45c93e2d8565048a1ceddc5db012673a7
luo <2407248257@qq.com>
2023-04-02 21:31:30 +0800

    controller

--- src/main/java/com/intellectrecovery/controller/DoctorController.java
+++ src/main/java/com/intellectrecovery/controller/DoctorController.java
@@ -7,6 +7,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Objects;
+
+import static com.intellectrecovery.constant.CacheKey.TOKEN_CACHE;
 
 @RestController
 @RequestMapping("/doctor")
@@ -20,27 +24,40 @@ public class DoctorController {
 
     @PostMapping("/login")
     public Result login(@RequestBody Doctor doctor) {
-        return null;
+        return doctorService.login(doctor.getUsername(), doctor.getPassword());
     }
 
     @DeleteMapping("/exit")
     public Result exit(@RequestBody String username) {
-        return null;
+        return doctorService.exit(username);
     }
 
     @PostMapping("/register")
     public Result register(@RequestBody Doctor doctor) {
-        return null;
+        return doctorService.register(doctor.getUsername(), doctor.getPassword());
     }
 
     @GetMapping("/getUser/{username}")
-    public Result getDoctorByUsername(@PathVariable String username) {
-        return null;
+    public Result getDoctorByUsername(@PathVariable String username, @RequestHeader("token") String token) {
+        String mode = stringRedisTemplate.opsForValue().get(TOKEN_CACHE + username);
+        if(Objects.equals(mode, token)) {
+            return doctorService.getDoctorByUsername(username);
+        } else {
+            return new Result(403, "鉴权失败", null);
+        }
     }
 
     @PutMapping("/changePassword")
-    public Result changePassword() {
-        return null;
+    public Result changePassword(@RequestBody HashMap<String, String> input, @RequestHeader("token") String token) {
+        String username = input.get("username");
+        String password = input.get("password");
+        String newPassword = input.get("newPassword");
+        String mode = stringRedisTemplate.opsForValue().get(TOKEN_CACHE + username);
+        if(Objects.equals(mode, token)) {
+            return doctorService.changePassword(username, password, newPassword);
+        } else {
+            return new Result(403, "鉴权失败", null);
+        }
     }
 
 }
--- src/main/java/com/intellectrecovery/controller/UserController.java
+++ src/main/java/com/intellectrecovery/controller/UserController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+import java.util.HashMap;
+import java.util.Objects;
+
+import static com.intellectrecovery.constant.CacheKey.TOKEN_CACHE;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -20,7 +25,7 @@ public class UserController {
 
     @PostMapping("/login")
     public Result login(@RequestBody User user) {
-        return null;
+        return userService.login(user.getUsername(), user.getPassword());
     }
 
     @DeleteMapping("/exit")
@@ -30,17 +35,30 @@ public class UserController {
 
     @PostMapping("/register")
     public Result register(@RequestBody User user) {
-        return null;
+        return userService.register(user.getUsername(), user.getPassword());
     }
 
     @GetMapping("/getUser/{username}")
-    public Result getUserByUsername(@PathVariable String username) {
-        return null;
+    public Result getUserByUsername(@PathVariable String username, @RequestHeader("token") String token) {
+        String mode = stringRedisTemplate.opsForValue().get(TOKEN_CACHE + username);
+        if(Objects.equals(mode, token)) {
+            return userService.getUserByUsername(username);
+        } else {
+            return new Result(403, "鉴权失败", null);
+        }
     }
 
     @PutMapping("/changePassword")
-    public Result changePassword() {
-        return null;
+    public Result changePassword(@RequestBody HashMap<String, String> input, @RequestHeader("token") String token) {
+        String username = input.get("username");
+        String password = input.get("password");
+        String newPassword = input.get("newPassword");
+        String mode = stringRedisTemplate.opsForValue().get(TOKEN_CACHE + username);
+        if(Objects.equals(mode, token)) {
+            return userService.changePassword(username, password, newPassword);
+        } else {
+            return new Result(403, "鉴权失败", null);
+        }
     }
 
 }
